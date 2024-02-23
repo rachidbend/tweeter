3556aef72b44a1a656a0fc574e9c036574b9a530
Rachid Bendaghor <rachidbendaghor@gmail.com>
2024-02-23 21:58:49 +0100

    started configuring the router. #1

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,30 @@ const router = createBrowserRouter([
   {
     path: '/',
     element: <div>app layout</div>,
+    children: [
+      // main pages
+      {
+        path: 'home',
+        element: <p>home page</p>,
+      },
+      {
+        path: 'explore',
+        element: <p>explore page</p>,
+      },
+      {
+        path: 'bookmarks',
+        element: <p>bookmarks page</p>,
+      },
+      // user pages
+      {
+        path: 'settings',
+        element: <p>settings page</p>,
+      },
+      {
+        path: 'profile',
+        element: <p>profile page</p>,
+      },
+    ],
   },
 ]);
 
