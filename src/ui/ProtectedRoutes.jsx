ebe702a3f132edfed6644924e2677c29380f4329
Rachid Bendaghor <rachidbendaghor@gmail.com>
2024-02-28 22:25:20 +0100

    added a spinner when logging in. #13

--- src/ui/ProtectedRoutes.jsx
+++ src/ui/ProtectedRoutes.jsx
@@ -2,6 +2,20 @@ import { useEffect } from 'react';
 import { useNavigate } from 'react-router';
 import { useUser } from '../hooks/authHooks/useUser';
 import toast from 'react-hot-toast';
+import styled from 'styled-components';
+import Spinner from './Spinner';
+
+const FullPage = styled.div`
+  width: 100%;
+  height: 100vh;
+  height: 100svh;
+
+  background-color: var(--color-grey-600);
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 
 // ProtectedRoute component ensures that only authenticated users can access certain routes.
 export default function ProtectedRoutes({ children }) {
@@ -23,7 +37,12 @@ export default function ProtectedRoutes({ children }) {
     [isLoadingUser, fetchStatus, isAuthenticated, navigate, user]
   );
 
-  if (isLoadingUser) return <p>loading user</p>;
+  if (isLoadingUser)
+    return (
+      <FullPage>
+        <Spinner />
+      </FullPage>
+    );
   if (userError) throw new Error(userError.message);
   if (!isAuthenticated) toast.error('You must be authenticated first!');
 
