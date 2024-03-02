52e0d3523e75eb01bd5f16f3a0887063858981b9
Rachid Bendaghor <rachidbendaghor@gmail.com>
2024-03-02 12:46:16 +0100

    adjusted user profile for mobile view #18

--- src/features/user/UserProfile.jsx
+++ src/features/user/UserProfile.jsx
@@ -4,6 +4,7 @@ import { useGetUserData } from '../../hooks/user/useGetUserData';
 import Spinner from '../../ui/Spinner';
 import toast from 'react-hot-toast';
 import { getUserData } from '../../services/apiUser';
+import { IoMdPersonAdd } from 'react-icons/io';
 
 const StyledUserProfile = styled.div`
   width: 100%;
@@ -22,6 +23,11 @@ const PageContainer = styled.div`
   padding: 0 5.4rem;
   max-width: calc(107.3rem + (5.4rem * 2));
   margin: 0 auto;
+
+  @media screen and (max-width: 450px) {
+    max-width: 100%;
+    padding: 0 1.66rem;
+  }
 `;
 const UserInfoContainer = styled.div`
   position: relative;
@@ -34,6 +40,15 @@ const UserInfoContainer = styled.div`
   display: flex;
   box-shadow: var(--shadow-100);
   gap: 2rem;
+
+  @media screen and (max-width: 450px) {
+    width: 100%;
+    flex-direction: column;
+    padding-bottom: 2.3rem;
+    top: -2rem;
+    gap: 1.1rem;
+    margin-bottom: 0rem;
+  }
 `;
 const UserAvatar = styled.img`
   width: 16rem;
@@ -46,6 +61,16 @@ const UserAvatar = styled.img`
   margin-bottom: -8.161rem;
   object-fit: cover;
   object-position: center;
+
+  @media screen and (max-width: 450px) {
+    flex-direction: column;
+    width: 12.243rem;
+    height: 12.243rem;
+    margin: 0 auto;
+    top: -9.6rem;
+    margin-bottom: -8.161rem;
+    border: 0.3rem solid var(--color-white);
+  }
 `;
 const UserName = styled.p`
   font-family: var(--font-poppings);
@@ -64,6 +89,12 @@ const Description = styled.p`
   color: var(--color-grey-300);
   text-align: left;
   max-width: 43rem;
+
+  @media screen and (max-width: 450px) {
+    width: 100%;
+    text-align: center;
+    margin-bottom: 1.3rem;
+  }
 `;
 
 const FollowButton = styled.button`
@@ -71,13 +102,17 @@ const FollowButton = styled.button`
   font-size: 1.2rem;
   font-weight: 500;
   letter-spacing: -0.035em;
-
+  align-self: flex-start;
   border: 2px solid var(--color-blue-100);
   border-radius: 0.4rem;
   cursor: pointer;
   padding: 0.8rem 2.4rem;
   color: var(--color-white);
   background-color: var(--color-blue-100);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 0.4rem;
 
   transition: background var(--transition-200), color var(--transition-200);
 
@@ -85,6 +120,16 @@ const FollowButton = styled.button`
     color: var(--color-blue-100);
     background-color: var(--color-white);
   }
+
+  @media screen and (max-width: 450px) {
+    margin: 0 auto;
+  }
+`;
+
+const FollowIcon = styled(IoMdPersonAdd)`
+  height: 1.4rem;
+  width: 1.4rem;
+  color: inherit;
 `;
 
 const PlaceHolder = styled.div`
@@ -96,16 +141,18 @@ const PlaceHolder = styled.div`
 const InfoContainer = styled.div`
   width: 100%;
 `;
-const UserInnerContainer = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  margin-bottom: 2.2rem;
-`;
+
 const UserAndStatContainer = styled.div`
   display: flex;
   align-items: center;
   gap: 2.6rem;
+  margin-bottom: 2.2rem;
+  @media screen and (max-width: 450px) {
+    flex-direction: column;
+    justify-content: center;
+    gap: 0.4rem;
+    margin-bottom: 1.4rem;
+  }
 `;
 const StatContainer = styled.div`
   display: flex;
@@ -137,28 +184,29 @@ function UserProfile() {
         <UserInfoContainer>
           <UserAvatar src={userProfile.avatar_image} />
           <InfoContainer>
-            <UserInnerContainer>
-              <UserAndStatContainer>
-                <UserName>{userProfile.user_name}</UserName>
-                <StatContainer>
-                  <Stat>
-                    {userProfile.following.length}
-                    <StatSpan>Following</StatSpan>
-                  </Stat>
-                  <Stat>
-                    {userProfile.followers.length}
-                    <StatSpan>Followers</StatSpan>
-                  </Stat>
-                </StatContainer>
-              </UserAndStatContainer>
-              <FollowButton>Follow</FollowButton>
-            </UserInnerContainer>
+            <UserAndStatContainer>
+              <UserName>{userProfile.user_name}</UserName>
+              <StatContainer>
+                <Stat>
+                  {userProfile.following.length}
+                  <StatSpan>Following</StatSpan>
+                </Stat>
+                <Stat>
+                  {userProfile.followers.length}
+                  <StatSpan>Followers</StatSpan>
+                </Stat>
+              </StatContainer>
+            </UserAndStatContainer>
+
             <Description>
               {userProfile.user_description
                 ? userProfile.user_description
-                : 'add a description!'}
+                : 'This user has not added a description yet!'}
             </Description>
           </InfoContainer>
+          <FollowButton>
+            <FollowIcon /> Follow
+          </FollowButton>
         </UserInfoContainer>
         <PlaceHolder></PlaceHolder>
       </PageContainer>
