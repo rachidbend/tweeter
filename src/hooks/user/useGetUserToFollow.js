3a8aa4e7a6d5b8772b5b90de56b6a1482988b955
Rachid Bendaghor <rachidbendaghor@gmail.com>
2024-03-18 14:46:32 +0000

    finished building the user to follow components. #44

---
+++ src/hooks/user/useGetUserToFollow.js
@@ -0,0 +1,17 @@
+import { useQuery } from '@tanstack/react-query';
+import { getUserToFollow } from '../../services/apiUser';
+
+export default function useGetUserToFollow(userId) {
+  const {
+    data: userToFollow,
+    isLoading,
+    error,
+  } = useQuery({
+    queryKey: ['userToFollow', userId],
+    queryFn: () => getUserToFollow({ userId: userId }),
+    enabled: !!userId,
+    staleTime: Infinity,
+  });
+
+  return { userToFollow, isLoading, error };
+}
